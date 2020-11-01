df27681a70a98a05cdd3719f040adee5aec67140
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-11-01 23:39:34 +0900

    WarmStartではないlassoのスクリプトを新たに書き直して別ファイルとした。

---
+++ linear_lasso_notWarmStart.R
@@ -0,0 +1,109 @@
+# Clear Working Memory
+rm(list = ls())
+
+# Function for determining formula type
+soft_th <- function(lambda, x) {
+  return(sign(x) * pmax(abs(x) - lambda, 0))
+}
+
+# Function for centralization
+centralize <- function(X, y, standardize = TRUE) {
+  X <- as.matrix(X)
+  n <- nrow(X)
+  p <- ncol(X)
+  X_bar <- array(dim = p)
+  X_sd <- array(dim = p)
+  
+  for (j in 1:p) {
+    X_bar[j] <- mean(X[, j])
+    X[, j] <- (X[, j] - X_bar[j])
+    X_sd[j] <- sqrt(var(X[, j]))
+    
+    if (standardize == TRUE) {
+      X[, j] <- X[, j] / X_sd[j]
+    }
+  }
+  
+  if (class(y) == "matrix") { # in case y is matrix
+    K <- ncol(y)
+    y_bar <- array(dim = K)
+    
+    for (k in 1:K) {
+      y_bar[k] <- mean(y[, k])
+      y[, k] <- y[, k] - y_bar[k]
+    }
+  } else { # in case y is vector
+    y_bar <- mean(y)
+    y <- y - y_bar
+  }
+  
+  return(list(X = X, y = y, X_bar = X_bar, X_sd = X_sd, y_bar = y_bar))
+}
+
+# Function for linear Lasso
+linear_lasso <- function(X, y, lambda = 0, beta = rep(0, ncol(X))) {
+  n <- nrow(X)
+  p <- ncol(X)
+  
+  res <- centralize(X, y)
+  X <- res$X
+  y <- res$y
+  
+  eps <- 1
+  beta_old <- beta
+  
+  while (eps > 0.001) {
+    for (j in 1:p) {
+      r <- y - as.matrix(X[, -j]) %*% beta[-j]
+      beta[j] <- soft_th(lambda, sum(r * X[, j]) / n) / (sum(X[, j] * X[, j]) / n)
+    }
+    eps <- max(abs(beta - beta_old))
+    print(eps)
+    beta_old <- beta
+  }
+  
+  beta <- beta / res$X_sd
+  beta_0 <- res$y_bar - sum(res$X_bar * beta)
+  
+  return(list(beta = beta, beta_0 = beta_0))
+}
+
+# Main routine
+
+df <- read.table("crime.txt")
+X <- df[, 3:7]
+y <- df[, 1]
+
+p <- ncol(X)
+lambda_seq <- seq(0, 200, 0.1)
+
+plot(lambda_seq, 
+     xlim = c(0, 200),
+     ylim = c(-10, 20), 
+     xlab = "lambda",
+     ylab = "beta", 
+     main = "各lambdaについての各係数の値",
+     type = "n",
+     col = "red")
+
+r <- length(lambda_seq)
+coef_seq <- array(dim = c(r, p))
+
+for (i in 1:r) {
+  coef_seq[i, ] <- linear_lasso(X, y, lambda_seq[i])$beta
+}
+
+for (j in 1:p) {
+  par(new = TRUE)
+  lines(lambda_seq, coef_seq[, j], col = j)
+}
+
+legend("topright",
+       legend = c("警察への年間資金",
+                  "25歳以上で高校を卒業した人の割合",
+                  "16~19歳で高校に通っていない人の割合", 
+                  "18~24歳で大学生の割合", 
+                  "25歳以上で4年制大学を卒業した人の割合"),
+       col = 1:p,
+       lwd = 2,
+       cex = 0.8)
